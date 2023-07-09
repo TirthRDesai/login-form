e43a6104ea9ff76c8a1768888b3703194c0be31e
tirthrdesai05 <tirthrdesai05@gmail.com>
2023-07-10 00:32:51 +0530

    3

--- LOGINFORM/settings.py
+++ LOGINFORM/settings.py
@@ -15,9 +15,10 @@ from pathlib import Path
 import os
 import mimetypes
 
+mimetypes.add_type("text/html", ".html", True)
 mimetypes.add_type("application/javascript", ".js", True)
 mimetypes.add_type("python", ".py", True)
-mimetypes.add_type("text/html", ".css", True)
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
