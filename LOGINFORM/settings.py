c9473a5b76ea7d400e350d08b012657db994f3ef
Tirth Desai <tirthrdesai05@gmail.com>
2023-07-09 23:59:16 +0530

    remove static

--- LOGINFORM/settings.py
+++ LOGINFORM/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'LOGINPAGE/templates/css'),
     os.path.join(BASE_DIR, 'LOGINPAGE/templates/js'),
