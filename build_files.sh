5c80e37db85540d9b99047cba419be20b8f6f6c5
tirthrdesai05 <tirthrdesai05@gmail.com>
2023-07-10 12:05:51 +0530

    3

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 # build_files.sh
-python -m pip install -–upgrade pip.
-python3 manage.py collectstatic --noinput
+python3.9 -m pip install -–upgrade pip.
+python3.9 manage.py collectstatic --noinput
