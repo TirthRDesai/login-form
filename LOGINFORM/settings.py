1d94acccec1426d3707cb334791acd633b851dbc
tirthrdesai05 <tirthrdesai05@gmail.com>
2023-07-10 12:25:02 +0530

    3

--- LOGINFORM/settings.py
+++ LOGINFORM/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'LOGINFORM.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -124,8 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 print("dir - " , BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'LOGINPAGE/templates/static')
 ]
