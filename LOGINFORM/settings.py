a4f488713c59cadab6230b33f8bf0f5a65e027e7
tirthrdesai05 <tirthrdesai05@gmail.com>
2023-07-10 14:18:22 +0530

    3

--- LOGINFORM/settings.py
+++ LOGINFORM/settings.py
@@ -127,9 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 print("dir - " , BASE_DIR)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'LOGINPAGE/templates/static')
-]
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
