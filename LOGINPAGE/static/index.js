bce5dd379876c5e723f43ed2936e82ed195da520
tirthrdesai05 <tirthrdesai05@gmail.com>
2023-07-10 22:10:16 +0530

    3

---
+++ LOGINPAGE/static/index.js
@@ -0,0 +1,31 @@
+window.onload = function(){
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+
+    signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+    });
+    console.log('PROGRAM STARTED');
+};
+
+
+
+function submitSignUp(){
+    var name = document.getElementById("nameInputSignUp").value;
+    var email = document.getElementById("emailInputSignUp").value;
+    var password = document.getElementById("passwordInputSignUp").value;
+    $.ajax({
+        method: 'POST',
+        url: '/checkData/SignUp',
+        data: {'name':name , 'email' : email, 'password': password},
+        dataType: 'json',
+        success: function(d){
+            console.log(d);
+        }   
+    });
+}
